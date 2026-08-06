6e5d817d4a910d21e60c4d2043f5fff0847e1c3f
Vinny <vinny8716@gmail.com>
2026-08-05 21:37:13 -0700

    Update availability_test.py

--- availability_test.py
+++ availability_test.py
@@ -1,88 +1,93 @@
 import subprocess
-import ipaddress
-from network_devices_list import network_devices
-import smtplib
 from email.message import EmailMessage
-import requests
 import datetime
+from network_devices_list import network_devices
+import paramiko
+import requests
+import smtplib
 i = 0
+#config
+ip_list = ['10.10.10.200', '10.10.10.210', '10.10.10.10', '10.10.10.20', '192.168.10.102', '192.168.20.102', '192.168.30.101', '192.168.10.101', '10.10.10.1', '10.10.10.100', '192.168.20.210', '192.168.30.210']
+device_name = network_devices[i]['Device Name']
+cmd = 'cat /etc/resolv.conf'
+username = 'ubuntu'
+password = 'ubuntu'
+temp_readout = ''
+bad_DNS = []
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 #email config
 stmp_s = 'smtp.d522.wgu.internal'
 port = 1025
 sender = 'ITDesk@mailhog.com'
-password = ''
+epassword = ''
 receiver = 'Stakeholders@mailhog.com'
+Timestamp = datetime.datetime.now()
+msg = EmailMessage()
+msg['Subject'] = f'DNS Configuration Alert: {network_devices[i]['Device Name']} {ip_list[i]})'
+msg['From'] = sender
+msg['To'] = receiver
+msg.set_content('Dear Network Administrator,\n'
+                '\n'
+                'This is an automated alert that the DNS configuration for the following device has been altered from the expected settings:\n'
+                '\n'
+                f'Device Name: {network_devices[i]['Device Name']}\n'
+                f'IP Address: {ip_list[i]}\n'
+                'Detected DNS Setting:\n'
+                f'{temp_readout}\n'
+                'Expected DNS Setting: 10.10.10.10 or 10.10.10.20 or loopback address\n'
+                f'Time Detected: {Timestamp}\n'
+                '\n'
+                'The system will attempt to automatically correct this configuration.\n'
+                '\n'
+                'Best regards,\n'
+                'Network Monitoring System\n')
 
 #ticket config
 token = 'vGkbXkGLqQSo7YLflp9DutuG8st4xdPPF7wnTcwB0FE'
 url = 'http://helpdesk.d522.wgu.internal:5000/api/tickets'
 headers = {'Authorization': f'Bearer {token}',
-           'Content-Type': 'application/json'
-           }
+            'Content-Type': 'application/json'
+            }
 payload = {
     "assigned_to": "John Pork",
-    "description": "Host is down, get the host up and running at the earliest convenience",
+    "description": "DNS settings changed!",
     "priority": "high",
     "requester_email": "ITDesk@mailhog.com",
-    "status": "open",
-    "title": f"{network_devices[i]['Device Address']} is down!"
-}
+    "status": "closed",
+    "title": f"{network_devices[i]['Device Name']} is down!"
+    }
 
 
+while i < len(network_devices):
+    try:
+        client.connect(hostname = ip_list[i], username = username, password = password, timeout = 10)
+        stdin, stdout, stderr = client.exec_command(cmd)
+        temp_readout = stdout.read().decode()
+        if '203.0.113.10' in temp_readout:
+            bad_DNS.append(network_devices[i]['Device Address'])
+            print(f'{ip_list[i]} DNS settings are wrong, email sent!')
+            try:
+                with smtplib.SMTP(stmp_s, port) as s:
+                    s.login(sender, epassword)
+                    s.send_message(msg)
+                print("email sent!")
+            except Exception as e:
+                print(f'ERROR: {e}')
+            command = ("sudo sed -i 's/^#\\?DNS=.*/DNS=10.10.10.10 10.10.10.20/' /etc/systemd/resolved.conf && "
+                        "sudo systemctl restart systemd-resolved && "
+                        "resolvectl status")
 
-
-#pinging ip and auto ticket/email
-valid_ip = []
-bad_ips = []
-for device in network_devices:
-    try :
-        ipaddress.ip_address(device['Device Address'])
-        valid_ip.append(device['Device Address'])
-    except ValueError:
-        pass
-while i < len(valid_ip):
-    host = valid_ip[i]
-    command = ['ping', '-c', '1', host]
-    result = subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    if result.returncode == 0:
-        print(host, 'Ping Successful')
-    else:
-        print(valid_ip[i], 'Ping Failed, sending email and creating ticket!')
-        Device_name = network_devices[i]['Device_name']
-        Timestamp = datetime.datetime.now()
-        msg = EmailMessage()
-        msg['Subject'] = f'Network Device Unavailable: {Device_name} ({host})'
-        msg['From'] = sender
-        msg['To'] = receiver
-        msg.set_content('Dear Network Administrator,'
-                        ''
-                        'This is an automated notification that the following network device is currently unavailable:'
-                        ''
-                        f'Device Name: {Device_name}'
-                        f'IP Address: {host}'
-                        f'Last Checked: {Timestamp}'
-                        ''
-                        'Please investigate this issue at your earliest convenience.'
-                        ''
-                        'Best regards,  '
-                        'Network Monitoring System'
-                        '')
-        try:
-            with smtplib.SMTP(stmp_s, port) as s:
-                s.login(sender, password)
-                s.send_message(msg)
-            print("email sent!")
-        except Exception as e:
-            print(f'ERROR: {e}')
-
-        response = requests.post(url, headers=headers, json=payload)
-
-        if response.status_code == 201:
-            print("Ticket created")
-            print(response.json)
-            print(f'Status Code: {response.status_code}')
+            stdin, stdout, stderr = client.exec_command(command)
+            print('DNS settings changed! Status:')
+            print(stdout.read().decode())
+            print(stderr.read().decode())
+            response = requests.post(url, headers=headers, json=payload)
         else:
-            print('something went wrong')
-            print(f'Status Code: {response.status_code}')
+            print(f'{ip_list[i]} is all good!')
+    except Exception as e:
+        print(f'Connection failed to {network_devices[i]['Device Name']}: {e}')
+    finally:
+        client.close()
     i = i + 1
